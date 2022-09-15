72c05583593e522d53f1aeab93c38db0df0d534a
Kamlesh Chhipa <kamleshchhipa@Kamleshs-MacBook-Air.local>
2022-09-15 07:11:05 +0530

    core app and database is set uped

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,6 +1,6 @@
-"""
-Calculator functions
-"""
+"""Calculator functions """
+
+
 def add(x, y):
     """Add x and y and return result."""
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,10 @@
-"""
-Sample tests
-"""
+""" Sample tests """
+
+
 from django.test import SimpleTestCase
 from app import calc
+
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
     def test_add_numbers(self):
